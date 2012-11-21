bdd42324347eba93d80ad39b73e1eaceca9fc258
Martin Rehfeld <martin.rehfeld@wooga.com>
2012-11-21 16:10:02 +0000

    take config exceptions into account

--- src/hcr_config.erl
+++ src/hcr_config.erl
@@ -6,6 +6,7 @@
 -export_type([type/0]).
 -type type() :: 'test'.
 
--spec incr1(type() | model()) -> non_neg_integer().
+-spec incr1(type() | model()) -> non_neg_integer() | no_return().
 incr1(test) -> 1;
-incr1({model, Type, _}) -> incr1(Type).
+incr1({model, Type, _}) -> incr1(Type);
+incr1(A) -> erlang:throw({hcr, no_config, [?MODULE, incr1, A]}).
