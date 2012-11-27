637ac9f6d47c5def26bbef7e8c5e51a82425799f
Martin Rehfeld <martin.rehfeld@wooga.com>
2012-11-27 10:22:29 +0000

    clean up parse transform

--- src/hcr_accessors.erl
+++ src/hcr_accessors.erl
@@ -5,14 +5,14 @@
 %%     -compile({parse_transform, hcr_accessors}).
 %%
 %% Use
+%%
 %%     ?ATTR_READER([{prop1, defaultvalue1}, ...]).
 %%     ?ATTR_ACCESSOR([{prop1, defaultvalue1}, ...]).
 %%     ?ATTR_WRITER([prop1, ...]).
+%%
 %% to generate getters and/or setters.
 
 -export([parse_transform/2]).
-
--export([init_defaults/3]).
 
 -record(state, { expected = any
                , exports  = []
@@ -24,56 +24,44 @@
 
 
 %% ===================================================================
-%% API functions
+%% Public functions
 %% ===================================================================
 
-%% @doc Initialize given properties with its defaults
-%% when they are not already set
--spec init_defaults(module(), M, [atom()]) -> M when M :: any_model().
-init_defaults(Mod, State, Properties) ->
-    Accessors = [ {fun Mod:P/1, fun Mod:P/2} || P <- Properties],
-    Fs = [ fun(S) -> W(S, R(S)) end || {R, W} <- Accessors ],
-    lists:foldl(fun (F, S) -> F(S) end, State, Fs).
-
-
 parse_transform(Forms, Options) ->
-    {Forms1, S} =
-        parse_trans:transform(fun xform_fun/4, #state{}, Forms, Options),
+    {ParsedForms, S} =
+        parse_trans:transform(fun parse_forms/4, #state{}, Forms, Options),
 
-    Forms2 = lists:foldl(fun ({M,A}, Acc) ->
-                                 parse_trans:export_function(M,A,Acc)
-                         end, Forms1, S#state.exports),
+    Fs = [fun (Fms) ->
+                  add_exports(Fms, S#state.exports)
+          end,
+          fun (Fms) ->
+                  append_forms(Fms,
+                      [ reader_form(S#state.module, P, Def, S#state.eof_line)
+                          || {P, Def} <- S#state.readers ]
+                  )
+          end,
+          fun (Fms) ->
+                  append_forms(Fms,
+                      [ writer_form(S#state.module, P, S#state.eof_line)
+                          || P <- S#state.writers ]
+                  )
+          end,
+          fun parse_trans:revert/1
+         ],
 
-    Forms3 = parse_trans:do_insert_forms(
-        below,
-        [ reader_form(S#state.module, Name, Default, S#state.eof_line)
-            || {Name, Default} <- S#state.readers ],
-        Forms2,
-        parse_trans:initial_context(Forms2, [])
-    ),
-
-    Forms4 = parse_trans:do_insert_forms(
-        below,
-        [ writer_form(S#state.module, Name, S#state.eof_line)
-            || Name <- S#state.writers ],
-        Forms3,
-        parse_trans:initial_context(Forms3, [])
-    ),
-    Result = parse_trans:revert(Forms4),
-    %% io:format("~n~n~nREWRITTEN~n~p~n", [Result]),
-    Result.
+    NewForms = lists:foldl(fun (F, Fms) -> F(Fms) end, ParsedForms, Fs),
+    %% io:format("~n~n~nREWRITTEN~n~p~n", [NewForms]),
+    NewForms.
 
 
 %% ===================================================================
 %% Internal functions
 %% ===================================================================
 
-xform_fun(eof_marker, {eof, Line}=Form, Ctx, S) ->
-    %% io:format("~p~n~p~n~n", [eof_marker, {Form, Ctx, S}]),
+parse_forms(eof_marker, {eof, Line}=Form, Ctx, S) ->
     {Form, true, S#state{eof_line = Line,
                          module   = parse_trans:context(module, Ctx)}};
-xform_fun(Type, Form, Ctx, S) ->
-    %% io:format("~p~n~p~n~n", [Type, {Form, Ctx, S}]),
+parse_forms(Type, Form, Ctx, S) ->
     case {parse_trans:context(function, Ctx), parse_trans:context(arity, Ctx)} of
         {attr_reader, 0}   -> parse_reader(Type, Form, Ctx, S);
         {attr_writer, 0}   -> parse_writer(Type, Form, Ctx, S);
@@ -89,10 +77,10 @@ parse_reader(clause, Form, _Ctx, #state{expected=clause}=S) ->
     {Form, true, S#state{expected=list}};
 parse_reader(list, Form, _Ctx, #state{expected=list}=S) ->
     {Form, true, S#state{expected=tuple}};
-parse_reader(tuple, {tuple, _, [{atom, _, Name}, Default]}=Form, _Ctx,
+parse_reader(tuple, {tuple, _, [{atom, _, P}, Def]}=Form, _Ctx,
              #state{expected=tuple, exports=Exports, readers=Readers}=S) ->
-    {Form, false, S#state{readers  = [{Name, Default} | Readers],
-                          exports  = [{Name, 1} | Exports],
+    {Form, false, S#state{readers  = [{P, Def} | Readers],
+                          exports  = [{P, 1} | Exports],
                           expected = list}};
 parse_reader(Type, Form, _Ctx, #state{expected=Expected}) ->
     parse_trans:error("Invalid attr_reader list", Form,
@@ -106,10 +94,10 @@ parse_writer(clause, Form, _Ctx, #state{expected=clause}=S) ->
     {Form, true, S#state{expected=list}};
 parse_writer(list, Form, _Ctx, #state{expected=list}=S) ->
     {Form, true, S#state{expected=atom}};
-parse_writer(atom, {atom, _, Name}=Form, _Ctx,
+parse_writer(atom, {atom, _, P}=Form, _Ctx,
              #state{expected=atom, exports=Exports, writers=Writers}=S) ->
-    {Form, false, S#state{writers  = [Name | Writers],
-                          exports  = [{Name, 2} | Exports],
+    {Form, false, S#state{writers  = [P | Writers],
+                          exports  = [{P, 2} | Exports],
                           expected = atom}};
 parse_writer(Type, Form, _Ctx, #state{expected=Expected}) ->
     parse_trans:error("Invalid attr_writer list", Form,
@@ -123,24 +111,24 @@ parse_accessor(clause, Form, _Ctx, #state{expected=clause}=S) ->
     {Form, true, S#state{expected=list}};
 parse_accessor(list, Form, _Ctx, #state{expected=list}=S) ->
     {Form, true, S#state{expected=tuple}};
-parse_accessor(tuple, {tuple, _, [{atom, _, Name}, Default]}=Form, _Ctx,
+parse_accessor(tuple, {tuple, _, [{atom, _, P}, Def]}=Form, _Ctx,
              #state{expected=tuple, exports=Exports,
                     readers=Readers, writers=Writers}=S) ->
-    {Form, false, S#state{readers  = [{Name, Default} | Readers],
-                          writers  = [Name | Writers],
-                          exports  = [{Name, 1} | [{Name, 2} | Exports]],
+    {Form, false, S#state{readers  = [{P, Def} | Readers],
+                          writers  = [P | Writers],
+                          exports  = [{P, 1} | [{P, 2} | Exports]],
                           expected = list}};
 parse_accessor(Type, Form, _Ctx, #state{expected=Expected}) ->
     parse_trans:error("Invalid attr_accessor list", Form,
         [{form, Form}, {got, Type}, {expected, Expected}]).
 
 
-%% @doc This is AST equivalent of getter/3's returned Fun
-reader_form(Tag, Name, Default, Line)
-  when is_atom(Tag) andalso is_atom(Name) andalso is_integer(Line) ->
+%% @doc AST of a attribute reader including a default value:
+%% Name({Mod, _, P}) -> proplists:get_value(Name, P, Default).
+reader_form(Mod, Name, Default, Line) ->
     {function, Line, Name, 1,
        [{clause,Line,
-         [{tuple,Line,[{atom,Line,Tag},{var,Line,'_'},{var,Line,'P'}]}],
+         [{tuple,Line,[{atom,Line,Mod},{var,Line,'_'},{var,Line,'P'}]}],
          [],
          [{call,Line,{remote,Line,{atom,Line,proplists},{atom,Line,get_value}},
            [{atom,Line,Name},
@@ -149,15 +137,16 @@ reader_form(Tag, Name, Default, Line)
            ]}]}]
     }.
 
-%% @doc This is AST equivalent of setter/2's returned Fun
-writer_form(Tag, Name, Line)
-  when is_atom(Tag) andalso is_atom(Name) andalso is_integer(Line) ->
+%% @doc AST of a attribute writer:
+%% Name({Mod, T, P}, V) -> {Mod, T, lists:keystore(Name, 1, P, {Name, V})}.
+writer_form(Mod, Name, Line) ->
     {function, Line, Name, 2,
        [{clause,Line,
-         [{tuple,Line,[{atom,Line,Tag},{var,Line,'T'},{var,Line,'P'}]}, {var,Line,'V'}],
+         [{tuple,Line,
+           [{atom,Line,Mod},{var,Line,'T'},{var,Line,'P'}]}, {var,Line,'V'}],
          [],
          [{tuple,Line,
-           [{atom,Line,Tag},
+           [{atom,Line,Mod},
             {var,Line,'T'},
             {call,Line,{remote,Line,{atom,Line,lists},{atom,Line,keystore}},
              [{atom,Line,Name},
@@ -166,3 +155,16 @@ writer_form(Tag, Name, Line)
               {tuple,Line,[{atom,Line,Name},{var,Line,'V'}]}
              ]}]}]}]
     }.
+
+add_exports(Forms, Exports) ->
+    lists:foldl(fun ({M,A}, Acc) ->
+                        parse_trans:export_function(M, A, Acc)
+                end, Forms, Exports).
+
+append_forms(Forms, AdditionalForms) ->
+    parse_trans:do_insert_forms(
+        below,
+        AdditionalForms,
+        Forms,
+        parse_trans:initial_context(Forms, [])
+    ).
